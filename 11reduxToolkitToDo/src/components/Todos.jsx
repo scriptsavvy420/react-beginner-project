31924b36bc3f8b81e1c72e0c23ab16b317937b4b
Script Savvy <scriptsavvy0420@gmail.com>
2023-10-19 23:58:01 +0530

    :)

--- 11reduxToolkitToDo/src/components/Todos.jsx
+++ 11reduxToolkitToDo/src/components/Todos.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
-    const todos = useSelector(state => state.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
 
   return (
     <>
-    <ul className="list-none">
+      <ul className="list-none">
         {todos.map((todo) => (
           <li
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
@@ -16,7 +16,7 @@ function Todos() {
           >
             <div className='text-white'>{todo.text}</div>
             <button
-             onClick={() => dispatch(removeTodo(todo.id))}
+              onClick={() => dispatch(removeTodo(todo.id))}
               className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
             >
               <svg
